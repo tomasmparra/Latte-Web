8367d89fcae6ca622b01545f8b89935b5d296717
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2020-07-04 17:29:31 +0200

    Commit

--- footer.php
+++ footer.php
@@ -2,6 +2,19 @@
 
   <footer class="footer" id="footer">
 
+    <!-- NAVIGATION BAR -->
+
+    <?php
+      $args = array(
+        'theme_location' => 'footer-menu',
+        'depth' => 0,
+        'container'	=> false,
+        'fallback_cb' => false,
+        'menu_class' => 'footer-menu',
+      );
+      wp_nav_menu($args);
+    ?>
+
   </footer>
   <sign class="signature">
     <p>&#60;&#47;&#62; by <a href="https://lattedev.com/" target="_blank" class="latteLink">Latte</a></p>
--- front-page.php
+++ front-page.php
@@ -1,7 +1,23 @@
 <?php get_header(); ?>
 
+<section class="ATF front_page_ATF section_padding">
+  <hgroup class="copy_hgroup text_center">
+    <h1 class="copy">DESARROLLO WEB</h1>
+    <h3 class="copy_2">¿Necesitas crear o mejorar tu página?</h3>
+    <h4 class="CTA text_right"><a class="CTA_link" href="#">-conoce nuestros precios y productos</a></h4>
+  </hgroup>
 
-<h1>front-page.php</h1>
-
+  <form action="" class="form front_page_form">
+    <div class="form_txt">
+      <h3 class="text_center">Cuentanos tu idea</h3>
+      <p class="">--Recibiras el <strong>precio en el día</strong></p>
+      <p class="">--Prespuestos <strong>a medida</strong></p>
+      <p class="">--Contarás con <strong>asesoramiento GRATUITO</strong></p>
+    </div>
+    <input type="text" class="form_input">
+    <input type="tel" class="form_input">
+    <textarea class="form_text_area"></textarea>
+  </form>
+</section>
 
 <?php get_footer(); ?>
